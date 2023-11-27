adc3fe3bfaeb88c15188282f8c9a4bc578bba501
Xolani Victory <psymbiotic@Xolanis-MacBook-Pro.local>
2023-11-27 15:05:41 -0600

    Added another array for additional freelancers and set interval

--- freelance.js
+++ freelance.js
@@ -1,8 +1,38 @@
 const freelancers = [
-    {name: "Alice", price: 30, job: "writer"},
-    {name: "Bob", price: 50, job: "teacher"}
-]
+    { name: "Alice", price: 30, job: "writer" },
+    { name: "Bob", price: 50, job: "teacher" }
+];
 
-// function avgPrice() {
+const additionalWorkers = [
+    {name: "Carol", price: 70, job: "programmer"},
+    {name: "Jessica", price: 50, job: "songwriter"},
+    {name: "Joshua", price: 100, job: "idol"},
+    {name: "Riley", price: 30, job: "writer"},
+    {name: "Brandon", price: 40, job: "MUA"}
+];
+let maxWorkers = 10;
+const addWorkerIntervalId = setInterval(addWorker, 3000);
 
-// }
+render()
+
+
+function render() {
+    const freelanceContainer = document.querySelector("#workers");
+
+    const freelanceElements = freelancers.map((person) => {
+        const element = document.createElement("li");
+        element.classList.add(person.name, person.job, person.price);
+    return element;
+});
+    freelanceContainer.replaceChildren(...freelanceElements);
+    console.log(freelanceElements);
+}
+
+function addWorker() {
+    const selectedWorker = additionalWorkers[Math.floor(Math.random() * freelancers.length)];
+freelancers.push(selectedWorker);
+if (freelancers.length >= maxWorkers) {
+    clearInterval(addWorkerIntervalId)
+  };
+render(); 
+}
