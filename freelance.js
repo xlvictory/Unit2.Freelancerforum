cb1d0d33a5ff9b7c2c91c324e4f36c71e5b98d6d
Xolani Victory <psymbiotic@Xolanis-MacBook-Pro.local>
2023-11-27 16:01:17 -0600

    Trying to figure out how to display code as text, will come back to later

--- freelance.js
+++ freelance.js
@@ -3,36 +3,48 @@ const freelancers = [
     { name: "Bob", price: 50, job: "teacher" }
 ];
 
+for(let i=0; i < freelancers.length; i++){
+    console.log(freelancers[i]);
+    
+}
+
 const additionalWorkers = [
-    {name: "Carol", price: 70, job: "programmer"},
-    {name: "Jessica", price: 50, job: "songwriter"},
-    {name: "Joshua", price: 100, job: "idol"},
-    {name: "Riley", price: 30, job: "writer"},
-    {name: "Brandon", price: 40, job: "MUA"}
+    { name: "Carol", price: 70, job: "programmer" },
+    { name: "Jessica", price: 50, job: "songwriter" },
+    { name: "Joshua", price: 100, job: "idol" },
+    { name: "Riley", price: 30, job: "writer" },
+    { name: "Brandon", price: 40, job: "MUA" }
 ];
-let maxWorkers = 10;
+let maxWorkers = 7;
 const addWorkerIntervalId = setInterval(addWorker, 3000);
 
 render()
 
 
 function render() {
+    
     const freelanceContainer = document.querySelector("#workers");
-
     const freelanceElements = freelancers.map((person) => {
         const element = document.createElement("li");
         element.classList.add(person.name, person.job, person.price);
-    return element;
-});
+        return element;
+    });
     freelanceContainer.replaceChildren(...freelanceElements);
-    console.log(freelanceElements);
+// let text = document.getElementById("workers").textContent;
+
 }
 
 function addWorker() {
     const selectedWorker = additionalWorkers[Math.floor(Math.random() * freelancers.length)];
-freelancers.push(selectedWorker);
-if (freelancers.length >= maxWorkers) {
-    clearInterval(addWorkerIntervalId)
-  };
-render(); 
+    freelancers.push(selectedWorker);
+    if (freelancers.length >= maxWorkers) {
+        clearInterval(addWorkerIntervalId)
+    };
+    render();
 }
+
+function avgPrice() {
+    
+    
+}let total = freelancers.price + additionalWorkers.price;
+console.log(total)
